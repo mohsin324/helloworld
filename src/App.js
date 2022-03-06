8e4fcf3dd91b255f000aeb514982d891673a81c5
Mohsin Munir <mohsinmunir324@gmail.com>
2022-03-06 23:47:24 +0500

    adding and updated app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import './App.css';
 const App = ()=> {
   return(
-    <div>
+    <div className='container'>
       <text>Hello World</text>
       <text>Developed by Mohsin Munir</text>
     </div>
